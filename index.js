69deaf0a68fb11a45148aafc8a8941642c1ddd2d
Dorian Macias <dorianrdba@gmail.com>
2024-09-25 14:25:37 -0700

    merged other branch

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 const readline = require("readline");
 const axios = require("axios");
 
-const databaseName = "my-db"; // this will be the name of your new database
-const databaseKey = "DATABASE_URL"; // this will be the name of the key in your env
+const databaseName = null; // this will be the name of your new database
+const databaseKey = null; // this will be the name of the key in your env
 const baseUrl = "https://api.render.com/v1";
 const key = process.env.API_KEY;
 
